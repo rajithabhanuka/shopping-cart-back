6001adc9632c07d53c1e08f800cfeab5b92e37ac
rajithabhanuka <rajithabhanuka1@gmail.com>
2021-06-21 00:15:02 +0530

    error code

--- src/main/java/com/code/shoppingcart/constants/ErrorCodes.java
+++ src/main/java/com/code/shoppingcart/constants/ErrorCodes.java
@@ -10,12 +10,6 @@ public class ErrorCodes {
     /** Record not found */
     public static final String X99002 = "X99002";
 
-    /** Error in request parameters */
-    public static final String X99003 = "X99003";
-
-    /** Error in Max uploading file size exceeded */
-    public static final String X99004 = "X99004";
-
     /** Error in Updating Password */
     public static final String X99005 = "X99005";
 }
